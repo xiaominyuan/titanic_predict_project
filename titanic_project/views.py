9f1b4f79519d907953b65db0a0861afaad687be6
xiaominyuan <yuanxiaomin1995@126.com>
2025-03-31 18:56:25 +0800

    all

--- titanic_project/views.py
+++ titanic_project/views.py
@@ -10,7 +10,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# 同步 API
 @csrf_exempt
 def titanic_sync(request):
     if request.method == "POST":
@@ -23,7 +22,6 @@ def titanic_sync(request):
             return JsonResponse({"error": str(e)}, status=400)
     return JsonResponse({"message": "Please use POST"}, status=405)
 
-# 异步 API
 @csrf_exempt
 def titanic_async(request):
     if request.method == "POST":
@@ -40,42 +38,14 @@ def titanic_async(request):
 
 
 
-# 结果获取 API
 def titanic_result(request, job_id):
 
     result = cache.get(job_id)
 
     if result is not None:
-        # 如果任务完成，返回结果
         return JsonResponse({"status": "success", "result": result})
     else:
-        # 如果任务仍在进行或未完成
         return JsonResponse({"status": "PENDING", "message": "Job not completed or job_id does not exist"}, status=404)
 
-
-# # 结果获取 API
-# def titanic_result(request, job_id):
-#     # 使用 Celery AsyncResult 获取任务状态和结果
-#     result = AsyncResult(job_id)
-#
-#     if result.state == 'SUCCESS':
-#         # 任务成功，返回结果
-#         return JsonResponse({
-#             'status': result.state,
-#             'result': result.result
-#         })
-#     elif result.state == 'FAILURE':
-#         # 任务失败
-#         return JsonResponse({
-#             'status': result.state,
-#             'error': str(result.result)  # 错误信息
-#         })
-#     else:
-#         # 任务尚未完成
-#         return JsonResponse({
-#             'status': result.state,
-#             'message': 'Job not completed or job_id does not exist'
-#         }, status=404)
-#
 def test_alive(request):
     return JsonResponse({"message": "i am alive"}, status=200)
